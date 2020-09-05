adee3f0ae1e3646b27a61cd95790f4f37f0b35ce
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2020-09-05 14:18:13 -0400

    all tasks complete

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
 
+
 // ⭐️ Example Challenge END ⭐️
 
 
@@ -34,7 +35,7 @@ function processFirstItem(stringList, callback) {
  *      counter1 uses closure.  Function counter is nested within counterMaker and is has access to all of the content within that function. It     could also reach out and grab variables that are not within the function. 
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
- *      Counter1 would be preferable when you are trying to protect the code.  Counter2 two would be good if you 
+ *      Counter1 would be preferable when you are trying to protect the code.  Counter2 two would be good if you wanted to use multiple counts.
  *
 */
 
@@ -60,11 +61,9 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(){
+  inning = () =>  Math.floor(Math.random()*3);
 
-    return Math.floor(Math.random()*3)
 
-}
 
 /* Task 3: finalScore()
 
@@ -84,8 +83,8 @@ function finalScore(callback, numberOfInnings){
 let home = 0;
 let away = 0;
 for(let i = 0; i <= numberOfInnings; i++){
-  home = home + inning();
-  away = away + inning();
+  home = home + callback();
+  away = away + callback();
 }
   return {Home: home, Away:away}
 }
@@ -113,31 +112,49 @@ Final Score: awayTeam - homeTeam */
 
 
 
-function getInningScore (callBack){
-  let home = 0;
-  let away = 0;
-for(let i = 0; i <= inningNumber; i++){
-  home =+ inning();
-  away =+ inning();
-}
-  return home, away;
+function getInningScore (cb){
+  let homeScore = cb();
+  let awayScore = cb();
+  let currentInning = {
+    Home: homeScore,
+    Away: awayScore
+  }
 
+  return currentInning;
 }
 
+console.log (getInningScore(inning));
 
 function scoreboard(callBack1, callBack2, inningNumber) {
-  if (inningNumber === 1){
-    return `1st inning: ${away} - ${home}`;
+
+  let finalHome = 0;
+  let finalAway = 0;
+
+
+  for (let i = 1; i <= inningNumber; i++){
+
+    let currentInning = callBack1(callBack2)
+  if (i === 1){
+    finalAway += currentInning.Away;
+    finalHome += currentInning.Home;
+    console.log(`1st inning: ${currentInning.Away} - ${currentInning.Home}`);
   }
-  else if (inningNumber === 2){
-    return `2nd inning: ${away} - ${home}`;
+  else if (i === 2){
+    finalAway += currentInning.Away;
+    finalHome += currentInning.Home;
+    console.log(`2nd inning: ${currentInning.Away} - ${currentInning.Home}`);
   }
-  else if (inningNumber === 3){
-    return `3rd inning: ${away} - ${home}`;
+  else if (i === 3){
+    finalAway += currentInning.Away;
+    finalHome += currentInning.Home;
+    console.log(`3rd inning: ${currentInning.Away} - ${currentInning.Home} `);
   }else{
-    return `${inningNumber}th inning: ${away} - ${home}`;
+    finalAway += currentInning.Away;
+    finalHome += currentInning.Home;
+    console.log(`${i}th inning: ${currentInning.Away} - ${currentInning.Home}`);
   }
-  return `Final Score: $`
+}
+   return `Final Score: ${finalAway} - ${finalHome} `
 }
 
 console.log(scoreboard(getInningScore, inning, 9));
